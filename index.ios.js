e0fe61eea1ebde368c4a4c5bc93bfd709dfb4cf4
Ralph Pina <ralph@everalbum.com>
2017-02-17 20:17:37 -0800

    app is linted

--- index.ios.js
+++ index.ios.js
@@ -4,50 +4,49 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View,
 } from 'react-native';
 
-export default class twiliorn extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
 });
+
+const twiliorn = () => (
+    <View style={styles.container}>
+        <Text style={styles.welcome}>
+            Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+            To get started, edit index.android.js
+        </Text>
+        <Text style={styles.instructions}>
+            Double tap R on your keyboard to reload,{'\n'}
+            Shake or press menu button for dev menu
+        </Text>
+    </View>
+);
+
+twiliorn.displayName = 'twiliorn';
+export default twiliorn;
 
 AppRegistry.registerComponent('twiliorn', () => twiliorn);
