83c42d6f49cbbbc9950a90ee0cc0b961ba763032
Ralph Pina <ralph@everalbum.com>
2017-02-19 21:23:17 -0800

    Added navigation to building custom view

---
+++ example/Home.js
@@ -0,0 +1,82 @@
+// @flow
+
+import React from 'react';
+import {
+    StyleSheet,
+    TouchableHighlight,
+    View,
+    Text,
+    Image,
+    // $FlowFixMe
+} from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        marginTop: 80,
+        alignItems: 'center',
+    },
+    heading: {
+        fontSize: 22,
+        marginTop: 50,
+        marginBottom: 20,
+        paddingHorizontal: 30,
+        textAlign: 'center',
+    },
+    button: {
+        height: 60,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'white',
+        marginTop: 30,
+        marginBottom: 20,
+    },
+    buttonText: {
+        fontSize: 20,
+        paddingLeft: 10,
+        paddingRight: 10,
+        color: 'red',
+    },
+});
+
+const Home = props => ({
+    navigate(name: string) {
+        props.navigator.push({
+            name,
+            passProps: {
+                name,
+            },
+        });
+    },
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Image
+                    // eslint-disable-next-line global-require
+                    source={require('./images/twilio_icon.png')}
+                />
+                <Text
+                    style={styles.heading}
+                >
+                    Hello. Try our two Twilio video examples.
+                </Text>
+                <TouchableHighlight
+                    style={styles.button}
+                    onPress={this.navigate('SimpleTwilioVideo')}
+                >
+                    <Text style={styles.buttonText}>Simple View</Text>
+                </TouchableHighlight>
+                <TouchableHighlight
+                    style={styles.button}
+                    onPress={this.navigate('CustomTwilioVideo')}
+                >
+                    <Text style={styles.buttonText}>Custom View</Text>
+                </TouchableHighlight>
+            </View>
+        );
+    },
+});
+
+Home.displayName = 'Home';
+export default Home;
--- index.android.js
+++ index.android.js
@@ -7,40 +7,37 @@
 import React from 'react';
 import {
     AppRegistry,
-    StyleSheet,
+    Navigator,
     // $FlowFixMe
 } from 'react-native';
 
-import TwilioVideoView from './TwilioVideoView';
+import Home from './example/Home';
+import SimpleTwilioVideo from './example/SimpleTwilioVideo';
+import CustomTwilioVideo from './example/CustomTwilioVideo';
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#F5FCFF',
+function renderScene(route, navigator) {
+    if (route.name === 'Main') {
+        return <Home navigator={navigator} {...route.passProps} />;
+    }
+    if (route.name === 'SimpleTwilioVideo') {
+        return <SimpleTwilioVideo navigator={navigator} {...route.passProps} />;
+    }
+    if (route.name === 'CustomTwilioVideo') {
+        return <CustomTwilioVideo navigator={navigator} {...route.passProps} />;
+    }
+    return null;
+}
+
+const App = () => ({
+    render() {
+        return (
+            <Navigator
+                style={{ flex: 1 }}
+                initialRoute={{ name: 'Main' }}
+                renderScene={renderScene}
+            />
+        );
     },
 });
 
-// ralph.pina+2@gmail.com
-const ACCESS_ONE = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiIsImN0eSI6InR3aWxpby1mcGE7dj0xIn0.eyJqdGkiOiJTS2E1MWFjMmZlNWQ0NTJmYTc1M2IzNWU3Njk1NzhkMjQxLTE0ODc1NTc2NTkiLCJpc3MiOiJTS2E1MWFjMmZlNWQ0NTJmYTc1M2IzNWU3Njk1NzhkMjQxIiwic3ViIjoiQUM1NzRmM2YxNDNjOWU2ZjQ3ZDNkMzliZWZkZWQ0MjQzZiIsImV4cCI6MTQ4NzU2MTI1OSwiZ3JhbnRzIjp7ImlkZW50aXR5IjoicmFscGgucGluYSsyQGdtYWlsLmNvbSIsInJ0YyI6eyJjb25maWd1cmF0aW9uX3Byb2ZpbGVfc2lkIjoiVlNkMTVkMTI4MGQwZGI1MTQ1YzQ1MzIzMDc3N2M1ZWJmOCJ9fX0.N_ae2XAC6NjkmtOfzo9jB3dx7komxUlDNhHbzF8B6EY';
-// ralph.pina+3@gmail.com
-const ACCESS_TWO = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiIsImN0eSI6InR3aWxpby1mcGE7dj0xIn0.eyJqdGkiOiJTS2E1MWFjMmZlNWQ0NTJmYTc1M2IzNWU3Njk1NzhkMjQxLTE0ODc1MjMyNzYiLCJpc3MiOiJTS2E1MWFjMmZlNWQ0NTJmYTc1M2IzNWU3Njk1NzhkMjQxIiwic3ViIjoiQUM1NzRmM2YxNDNjOWU2ZjQ3ZDNkMzliZWZkZWQ0MjQzZiIsImV4cCI6MTQ4NzUyNjg3NiwiZ3JhbnRzIjp7ImlkZW50aXR5IjoicmFscGgucGluYSszQGdtYWlsLmNvbSIsInJ0YyI6eyJjb25maWd1cmF0aW9uX3Byb2ZpbGVfc2lkIjoiVlNkMTVkMTI4MGQwZGI1MTQ1YzQ1MzIzMDc3N2M1ZWJmOCJ9fX0.HH0RedqsN-eQdgEEWMWQXtsQDh9hHiIL11XnIDwiZWs';
-
-/**
- * You must provide a Twilio Access Token to connect to the Video service
- * @type {string}
- */
-const TWILIO_ACCESS_TOKEN = ACCESS_ONE;
-
-const twiliorn = () => (
-    <TwilioVideoView
-        twilioAccessToken={TWILIO_ACCESS_TOKEN}
-        style={styles.container}
-    />
-);
-
-twiliorn.displayName = 'twiliorn';
-export default twiliorn;
-
-AppRegistry.registerComponent('twiliorn', () => twiliorn);
+AppRegistry.registerComponent('twiliorn', () => App);
